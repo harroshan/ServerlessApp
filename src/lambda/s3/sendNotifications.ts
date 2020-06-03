f16cb392acab4a5f02cf30c49a02bdfe67b96d8f
harroshan <harroshanmakkar04@gmail.com>
2020-06-03 18:19:08 +0530

    Notification service added

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda'
+import { SNSHandler, SNSEvent, S3Event, S3Handler } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 
@@ -10,33 +10,32 @@ const apiId = process.env.API_ID
 
 const connectionParams = {
   apiVersion: "2018-11-29",
-  endpoint: `${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
+  endpoint: `${apiId}.execute-api.ap-southeast-1.amazonaws.com/${stage}`
 }
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
 
-export const handler: SNSHandler = async (event: SNSEvent) => {
-  console.log('Processing SNS event ', JSON.stringify(event))
-  for (const snsRecord of event.Records) {
-    const s3EventStr = snsRecord.Sns.Message
-    console.log('Processing S3 event', s3EventStr)
-    const s3Event = JSON.parse(s3EventStr)
+// export const handler: SNSHandler = async (event: SNSEvent) => {
+  // console.log('Processing SNS event ', JSON.stringify(event))
+  // for (const snsRecord of event.Records) {
+    // const s3EventStr = snsRecord.Sns.Message
+    // console.log('Processing S3 event', s3EventStr)
+    // const s3Event = JSON.parse(s3EventStr)
 
-    await processS3Event(s3Event)
-  }
-}
-
-async function processS3Event(s3Event: S3Event) {
-  for (const record of s3Event.Records) {
+    // await processS3Event(s3Event)
+//   }
+// }
+export const handler: S3Handler = async (event: S3Event) => {
+  for(const record of event.Records){
     const key = record.s3.object.key
-    console.log('Processing S3 item with key: ', key)
-
+    console.log('Processing S3 item with key ', key)
+    
     const connections = await docClient.scan({
-        TableName: connectionsTable
+      TableName: connectionsTable
     }).promise()
 
     const payload = {
-        imageId: key
+      imageId: key
     }
 
     for (const connection of connections.Items) {
@@ -45,6 +44,27 @@ async function processS3Event(s3Event: S3Event) {
     }
   }
 }
+
+
+// async function processS3Event(s3Event: S3Event) {
+//   for (const record of s3Event.Records) {
+//     const key = record.s3.object.key
+//     console.log('Processing S3 item with key: ', key)
+
+//     const connections = await docClient.scan({
+//         TableName: connectionsTable
+//     }).promise()
+
+//     const payload = {
+//         imageId: key
+//     }
+
+//     for (const connection of connections.Items) {
+//         const connectionId = connection.id
+//         await sendMessageToClient(connectionId, payload)
+//     }
+//   }
+// }
 
 async function sendMessageToClient(connectionId, payload) {
   try {
