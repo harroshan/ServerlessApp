715f2fd4dda9f077c6a9ff0f897ec0a3707e2edd
harroshan <harroshanmakkar04@gmail.com>
2020-06-07 08:21:13 +0530

    Baic Serverless App Ready with Authentication

---
+++ serverless-frontend/src/config.ts
@@ -0,0 +1,8 @@
+export const apiEndpoint = 'https://szk08v70fb.execute-api.ap-southeast-1.amazonaws.com/dev'
+
+export const authConfig = {
+  domain: 'harroshan.auth0.com',
+  // clientId: 'nl3ZGeQc6dkxaPJQTAENaMeuTVHs4kPm',
+  clientId: 's3At3p4tmve4GuH65Bh05pCYHxakumPf',
+  callbackUrl: 'http://localhost:3000/callback'
+}
