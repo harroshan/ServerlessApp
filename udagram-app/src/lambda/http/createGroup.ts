0f3a22d11edba4b4e7a92490cf808dba857a0393
harroshan <harroshanmakkar04@gmail.com>
2020-06-07 22:53:51 +0530

    ports and adapters basic

--- udagram-app/src/lambda/http/createGroup.ts
+++ udagram-app/src/lambda/http/createGroup.ts
@@ -1,39 +1,67 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
-import * as uuid from 'uuid'
-import { getUserId } from '../../auth/utils'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
-const groupsTable = process.env.GROUPS_TABLE
+import { CreateGroupRequest } from '../../requests/CreateGroupRequest'
+import { createGroup } from '../../businessLogic/groups'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
-  const itemId = uuid.v4()
 
-  const parsedBody = JSON.parse(event.body)
+  const newGroup: CreateGroupRequest = JSON.parse(event.body)
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
-  const newItem = {
-    id: itemId,
-    userId: getUserId(jwtToken),
-    ...parsedBody
-  }
-
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise()
+  const newItem = await createGroup(newGroup, jwtToken)
 
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
       newItem
     })
   }
 }
+
+// import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+// import 'source-map-support/register'
+// import * as AWS  from 'aws-sdk'
+// import * as uuid from 'uuid'
+// import { getUserId } from '../../auth/utils'
+
+// const docClient = new AWS.DynamoDB.DocumentClient()
+// const groupsTable = process.env.GROUPS_TABLE
+
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//   console.log('Processing event: ', event)
+//   const itemId = uuid.v4()
+
+//   const parsedBody = JSON.parse(event.body)
+//   const authorization = event.headers.Authorization
+//   const split = authorization.split(' ')
+//   const jwtToken = split[1]
+
+//   const newItem = {
+//     id: itemId,
+//     userId: getUserId(jwtToken),
+//     ...parsedBody
+//   }
+
+//   await docClient.put({
+//     TableName: groupsTable,
+//     Item: newItem
+//   }).promise()
+
+//   return {
+//     statusCode: 201,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*'
+//     },
+//     body: JSON.stringify({
+//       newItem
+//     })
+//   }
+// }
--- udagram-app/src/lambda/http/getGroups.ts
+++ udagram-app/src/lambda/http/getGroups.ts
@@ -1,19 +1,11 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-
-const groupsTable = process.env.GROUPS_TABLE
+import { getAllGroups } from '../../businessLogic/groups';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
 
-  const result = await docClient.scan({
-    TableName: groupsTable
-  }).promise()
-
-  const items = result.Items
+  const groups = await getAllGroups()
 
   return {
     statusCode: 200,
@@ -21,7 +13,34 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items
+      items: groups
     })
   }
 }
+// import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+// import 'source-map-support/register'
+// import * as AWS  from 'aws-sdk'
+
+// const docClient = new AWS.DynamoDB.DocumentClient()
+
+// const groupsTable = process.env.GROUPS_TABLE
+
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//   console.log('Processing event: ', event)
+
+//   const result = await docClient.scan({
+//     TableName: groupsTable
+//   }).promise()
+
+//   const items = result.Items
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*'
+//     },
+//     body: JSON.stringify({
+//       items
+//     })
+//   }
+// }
